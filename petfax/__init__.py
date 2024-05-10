080cfc32d5e11d7009a4b20b01da953e9783097d
Andrew Thomas <athomas94@gmail.com>
2024-05-09 21:29:49 -0400

    Added all of PetFax up to day 6 activities

---
+++ petfax/__init__.py
@@ -0,0 +1,15 @@
+# Import
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return ('Hello, PetFax')
+    
+    # Import the Blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+    
+    return app
